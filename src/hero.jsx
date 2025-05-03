9d87358934f0ca53fd32aa4e0ae10cc7a3a14f1c
Ismail Ahmad <ismailahmad0505@gmail.com>
2025-05-03 10:50:39 +0500

    list-style-image

--- src/hero.jsx
+++ src/hero.jsx
@@ -35,13 +35,10 @@ function Hero() {
         <p ref={heroText} className="hero-text">Elevate Your Brand with our <i>D3G</i> approach, Your Complete Digital Journey</p>
         <ul className="hero-list">
           <li ref={heroListFirst} className="hero-list-first">
-            <svg width="19" height="14" viewBox="0 0 19 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.39939 13.5005L0 7.10107L1.59985 5.50123L6.39939 10.3008L16.7002 0L18.3 1.59985L6.39939 13.5005Z" fill="#FF7043"/></svg>
                 <i>Design</i></li>
           <li ref={heroListSecond} className="hero-list-second">
-            <svg width="19" height="14" viewBox="0 0 19 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.39939 13.5005L0 7.10107L1.59985 5.50123L6.39939 10.3008L16.7002 0L18.3 1.59985L6.39939 13.5005Z" fill="#FF7043"/></svg>
                 <i>Develop &amp; Deploy</i></li>
           <li ref={heroListThird} className="hero-list-third">
-            <svg width="19" height="14" viewBox="0 0 19 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.39939 13.5005L0 7.10107L1.59985 5.50123L6.39939 10.3008L16.7002 0L18.3 1.59985L6.39939 13.5005Z" fill="#FF7043"/></svg>
                 <i>Grow</i> your Business</li>
         </ul>
         <CTAButton ref={heroCTA} classNames={"cta-button hero-cta"} onclick={() => {window.location.href='mailto:ismail.ahmad@designgravitas.com'}} text="Get a Free Quote" />
