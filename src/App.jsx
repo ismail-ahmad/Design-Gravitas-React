7c93c1432a292a8768aeaf67c5fe7930d9acd32e
Ismail Ahmad <ismailahmad0505@gmail.com>
2025-05-04 18:00:30 +0500

    test branch for react learning

--- src/App.jsx
+++ src/App.jsx
@@ -9,46 +9,11 @@ import Contact from './contact.jsx';
 import Footer from './footer.jsx';
 
 function App() {
-  const [instate, setinstate] = useState(false);
-  const servicesSection = useRef(null);
-  const wcuSection = useRef(null);
-  const logos = useRef(null);
-  const contact = useRef(null);
-  useEffect(() => {
-    const options = {
-      root: null,
-      threshold: 0.95,
-    };
-    let Observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if(entry.isIntersecting){
-          console.log("intersecting!");
-          entry.target.querySelectorAll(" .service-container ").forEach((el) => {
-            el.classList.add("desktopshow");
-          });
-        }
-      });
-    }
-    ,options);
-
-    Observer.observe(servicesSection.current);
-
-  },);
-
-  // ref={wcuSection}
-  // ref={logos}
-  // ref={contact}
-
 
   return(
     <>
-      <Header />
-      <Hero />
-      <Services ref={servicesSection} />
-      <WCU  />
-      <Clients  />
-      <Contact  />
-      <Footer />
+      <h1>hey</h1>
+      
     </>
   );
 }
