defb758a9c0f96aa2c601dd7624894c6a5de4bf6
Ismail Ahmad <ismailahmad0505@gmail.com>
2025-05-22 22:50:47 +0500

    Basic React Website Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from "react";
-import Header from "./header";
-import { SharedIntersectionObserverProvider } from './sharedIntersectionObserver';
+import React from 'react';
+import Layout from './Layout';
+import { BrowserRouter, Router, Route } from 'react-router-dom';
 
 const App = () => {
   return(
-    <SharedIntersectionObserverProvider threshold={1}>
-    <Header />
-    </SharedIntersectionObserverProvider>
+    <BrowserRouter>
+      <Router>
+        <Route path='/' element='<Layout />'>
+          <Route index element=''></Route>
+          <Route path='/services' element=''></Route>
+          <Route path='/contact' element=''></Route>
+          <Route path='/about' element=''></Route>
+        </Route>
+      </Router>
+    </BrowserRouter>
   );
 }
 
